0e2e67970679aec1dd157ddceff8a9aebb53ffd4
Serge Nm <nmugishaserge@gmail.com>
2022-06-29 10:33:18 +0200

    code refractor

--- src/__tests__/users.test.tsx
+++ src/__tests__/users.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent, screen } from '../../test/testUtils';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Users from '../components/github-username/Users';
 
 
